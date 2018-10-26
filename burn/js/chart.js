cae1160fa771b091eaeae361fccd8a5854ef154b
Lucas santos <Lucas.sousa@catho.com>
2018-10-26 16:43:59 -0300

    Formatação de data

--- burn/js/chart.js
+++ burn/js/chart.js
@@ -1,7 +1,6 @@
 
 function createDataset(color, name, dados, ) {
     return {
-        
         label: name,
         backgroundColor: color,
         borderColor: color,
@@ -10,136 +9,63 @@ function createDataset(color, name, dados, ) {
     }
 }
 
-/*
-//chart
-var mySpecialChart = null;
+$(document).ready(function () {
+    var pontos = [];
+    var extras = [];
+    var dias = [];
 
-var config = {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [
-            createDataset(window.chartColors.red, 'Points', ),
-            createDataset(window.chartColors.blue, 'Extra Tasks', ),
-            createDataset(window.chartColors.black, 'metrica', ),
+    $.ajax({
+        url: 'http://localhost:3000/burn',
+        type: 'GET', //POST GET DELETE PUT
+        //async: true,
+        //data: {chave: valor},
+        success: function (resposta) {
+            $.each(resposta, function (indice, valores) {
+                pontos.push(valores.points);
+                extras.push(valores.extra_task);
+                dias.push(valores.data.substring(0,10));
+            });
 
-        ]
+            var config = {
+                type: 'line',
+                data: {
+                    labels: dias,
+                    datasets: [
+                        createDataset(window.chartColors.red, 'Points', pontos),
+                        createDataset(window.chartColors.blue, 'Extra Tasks', extras),
+                        //createDataset(window.chartColors.black, 'metrica',[20,0] ),
+                    ]
 
-        
-    },
+                },
+
+            };
+
+            var ctx = document.getElementById('canvas').getContext('2d');
+            mySpecialChart = new Chart(ctx, config);
+            window.myLine = mySpecialChart;
 
 
-    options: {
-        responsive: true,
-        title: {
-            display: true,
-            text: 'Burndown'
         },
-        tooltips: {
-            mode: 'index',
-            intersect: false,
-        },
-        hover: {
-            mode: 'nearest',
-            intersect: true
-        },
-        scales: {
-            xAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
+        error: function (error) {
+            //Tratar Erro
         }
-    }
-};
-
-
-
-
-
-
-window.onload = function () {
-
-    var ctx = document.getElementById('canvas').getContext('2d');
-    mySpecialChart = new Chart(ctx, config);
-    window.myLine = mySpecialChart;
-};
-
-
-
-
-/*
-document.getElementById('randomizeData').addEventListener('click', function () {
-    config.data.datasets.forEach(function (dataset) {
-        dataset.data = dataset.data.map(function () {
-            return randomScalingFactor();
-        });
-
     });
 
-    window.myLine.update();
-});
-*/
-/*
-var colorNames = Object.keys(window.chartColors);
-document.getElementById('addDataset').addEventListener('click', function () {
-    var colorName = colorNames[config.data.datasets.length % colorNames.length];
-    var newColor = window.chartColors[colorName];
-    var newDataset = {
-        label: 'Dataset ' + config.data.datasets.length,
-        backgroundColor: newColor,
-        borderColor: newColor,
-        data: [],
-        fill: false
-    };
+    $('#pega-form').on('submit', function () {
+        var form = this;
+        console.log(form);
+        $.ajax({
+            url: 'http://localhost:3000/burn',
+            type: 'POST', //POST GET DELETE PUT
+            async: true,
+            data: $(form).serialize(),
+            success: function (resposta) {
+                console.log(resposta);
 
-    for (var index = 0; index < config.data.labels.length; ++index) {
-        newDataset.data.push(randomScalingFactor());
-    }
-
-    config.data.datasets.push(newDataset);
-    window.myLine.update();
-});
-
-
-
-
-document.getElementById('addData').addEventListener('click', function () {
-    if (config.data.datasets.length > 0) {
-        var month = MONTHS[config.data.labels.length % MONTHS.length];
-        config.data.labels.push(month);
-
-        config.data.datasets.forEach(function (dataset) {
-            dataset.data.push(randomScalingFactor());
+            },
+            error: function (error) {
+                //Tratar Erro
+            }
         });
-
-        window.myLine.update();
-    }
-});
-
-/*
-document.getElementById('removeDataset').addEventListener('click', function () {
-    config.data.datasets.splice(0, 1);
-    window.myLine.update();
-});
-
-document.getElementById('removeData').addEventListener('click', function () {
-    config.data.labels.splice(-1, 1); // remove the label first
-
-    config.data.datasets.forEach(function (dataset) {
-        dataset.data.pop();
     });
-
-    window.myLine.update();
 });
-
-*/
